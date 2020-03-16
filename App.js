39509c7e51a7c1cd40a133f11651ab9898575eb1
jeswinsimon <jeswinsimon@gmail.com>
2020-03-16 10:03:53 +0530

    V1 build

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,60 +14,121 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  Alert,
+  ActivityIndicator,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { WebView } from 'react-native-webview';
+import DocumentPicker from 'react-native-document-picker';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+async function uploadFile() {
+  try {
+    const res = await DocumentPicker.pick({
+      type: [DocumentPicker.types.allFiles],
+    });
+    Alert.alert(
+      `${res.uri},
+      ${res.type}, // mime type
+      ${res.name},
+      ${res.size}`
+    );
+  } catch (err) {
+    if (DocumentPicker.isCancel(err)) {
+      // User cancelled the picker, exit any dialogs or menus and move on
+    } else {
+      throw err;
+    }
+  }
+}
 
 const App: () => React$Node = () => {
+  const [isInitial, setInitial] = useState(true);
+  const [isStep1, setStep1] = useState(false);
+  const [isWebView, setWebView] = useState(false);
+  const [isStep2, setStep2] = useState(false);
+  const loadStep1 = () => {
+    setInitial(false);
+    setStep1(true);
+  };
+  const loadWebView = () => {
+    setStep1(false);
+    setWebView(true);
+  }
+  const resetStates = () => {
+    setInitial(true);
+    setStep1(false);
+    setWebView(false)
+    setStep2(false);
+  };
+  const renderLoadingView = () => {
+    return (
+      <ActivityIndicator
+        color='#bc2b78'
+        style={{ position: "absolute", left: 0, right: 0, bottom: 0, top: 0, }}
+        size="large"
+        hidesWhenStopped={true}
+      />
+    );
+  }
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      {/* <SafeAreaView> */}
+      {isInitial && <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Step 1</Text>
+          <Text style={styles.sectionDescription}>We need access to your location history to be able to cross check data. Please tap on the below button to request Location History from Google</Text>
+          <Button
+            title="Request Location History"
+            onPress={() => loadStep1()}
+          />
+        </View>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Step 2</Text>
+          <Text style={styles.sectionDescription}>If you have completed Step 1 you will soon receive an email from Google with your data. Please follow the link in the email and download a zip file. Please tap on the below button to upload the file.</Text>
+
+          <Button
+            title="Upload Location History"
+            onPress={() => uploadFile()}
+          />
+        </View>
+      </ScrollView>}
+      {isStep1 && <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}>
+
+        <View style={styles.sectionContainer}>
+          <Button
+            title="Back"
+            onPress={() => resetStates()}
+          />
+          <Text style={styles.sectionTitle}>Request Location History</Text>
+          <Text style={styles.sectionDescription}>
+            1. Tap on the below button to load Google Takeout{"\n"}
+            2. Login using your Google Account</Text>
+          <Button
+            title="Request Location History"
+            onPress={() => loadWebView()}
+          />
+        </View>
+      </ScrollView>}
+      {isWebView && <>
+        <Button
+          title="Close"
+          onPress={() => resetStates()}
+        />
+        <WebView
+          source={{ uri: 'https://takeout.google.com/settings/takeout/custom/location_history' }}
+          style={{ marginTop: 20 }}
+          renderLoading={renderLoadingView} startInLoadingState={true}
+        />
+      </>}
+      {/* </SafeAreaView> */}
     </>
   );
 };
@@ -84,16 +145,18 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
   },
   sectionContainer: {
-    marginTop: 32,
+    marginVertical: 32,
     paddingHorizontal: 24,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
     color: Colors.black,
+    textAlign: 'center',
+    paddingTop: 10,
   },
   sectionDescription: {
-    marginTop: 8,
+    marginVertical: 20,
     fontSize: 18,
     fontWeight: '400',
     color: Colors.dark,
