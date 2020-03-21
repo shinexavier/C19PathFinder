e0b47de411b3c56380622d3855966d9f1010bebb
admin <admin@admins-MacBook-Pro.local>
2020-03-21 16:56:00 +0530

    adding help line numbers

--- mobileApp/src/HelplineNumbersScreen.js
+++ mobileApp/src/HelplineNumbersScreen.js
@@ -1,65 +1,134 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar, StyleSheet, SectionList, View, Text } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-import axios from "axios";
-import Config from "react-native-config";
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  StatusBar,
+  Linking,
+  Image,
+} from 'react-native';
 
-function Item({title}) {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-    );
-  }
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const TestCentresScreen: () => React$Node = () => {
-    const [testingSites, setTestingSites] = useState();
+import { Container, Header, Content, DeckSwiper, Card, CardItem, Body, Text, H1, Left, Right, Title, 
+  List,
+  ListItem} from 'native-base';
 
-    useEffect(() => {
-        console.log("use effect")
-        axios
-            .get(
-                `${Config.API_URL}/testingsites`
-            )
-            .then(({ data }) => {
-                console.log(data.sites)
-                setTestingSites(data.sites);
-            })
-            .catch(error => console.log(error));
-    }, []);
 
-    return (
-        <View style={styles.container}>
-            <StatusBar barStyle="dark-content" />
-            {testingSites && <SectionList
-                sections={testingSites}
-                keyExtractor={(item, index) => item + index}
-                renderItem={({ item }) => <Item title={item} />}
-                renderSectionHeader={({ section: { state } }) => (
-                    <Text style={styles.header}>{state}</Text>
-                )}
-            />}
+const HelpLineNumbers: () => React$Node = () => {
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      {/* <SafeAreaView> */}
+      <Container>
+      <Header>
+          <Left/>
+          <Body>
+            <Title>Help Line Numbers</Title>
+          </Body>
+        </Header>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}>
+        <View style={styles.sectionContainer}>
+
+              <Card style={{ width: '100%' }}>
+                <CardItem header style={styles.cardTitle}>
+                  <H1 style={styles.cardTitleText}>CENTRAL HELP LINE NUMBER</H1>
+                </CardItem>
+                <CardItem>
+                    <Text style={{fontWeight: 'bold'}} onPress={() => Linking.openURL('tel:+91-11-23978046')}>+91-11-23978046</Text> 
+                </CardItem>
+              </Card>
+
+              <Card style={{ width: '100%' }}>
+                <CardItem header style={styles.cardTitle}>
+                  <H1 style={styles.cardTitleText}>STATE HELP LINE NUMBERS</H1>
+                </CardItem>
+                <CardItem>
+
+                </CardItem>
+              </Card>
+              
         </View>
-    );
+      </ScrollView>
+      </Container>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginHorizontal: 16,
-        marginVertical: 10,
-      },
-      item: {
-        padding: 5,
-        marginVertical: 4,
-      },
-      header: {
-        fontSize: 20,
-        fontWeight: 'bold'
-      },
-      title: {
-        fontSize: 24,
-      },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginVertical: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+    textAlign: 'center',
+    paddingTop: 10,
+  },
+  sectionDescription: {
+    marginVertical: 20,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  card: {
+
+  },
+  cardTitle: {
+    borderBottomWidth: 1,
+    borderColor: '#a50a18'
+  },
+  cardTitleText: {
+    color: '#a50a18',
+    fontSize: 22
+  },
+  cardBody: {
+
+  },
+  answeredBy: {
+    color: '#a50a18'
+  },
+  cardFooter: {
+
+  },
+  referenceLink: {
+    color: '#1c20c2',
+    fontSize: 18,
+  }
 });
 
-export default TestCentresScreen;
+export default HelpLineNumbers;
