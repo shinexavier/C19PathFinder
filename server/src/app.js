df7023eb90cd73f7e4063f0dda13204959ab8e09
bvaisakh <vaisakhb12@gmail.com>
2020-03-21 19:05:13 +0530

    Linting changes

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,6 @@
 var express = require('express');
 var logger = require('morgan');
 var glob = require('glob');
-var path = require('path');
 var config = require('./../resources/config');
 
 
@@ -14,8 +13,8 @@ var app = express();
 
 // Load mongoose models
 var models = glob.sync(config.root + '/src/models/*.js');
-models.forEach(function (model) {
-    require(model);
+models.forEach(function(model) {
+  require(model);
 });
 
 
@@ -24,34 +23,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 var controllers = glob.sync(config.root + '/src/controllers/*.js');
-controllers.forEach(function (controller) {
-    require(controller)(app);
+controllers.forEach(function(controller) {
+  require(controller)(app);
 });
 
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.json({
-            message: err.message,
-            error: err,
-            title: 'error',
-        });
-    });
-}
-
-app.use(function (err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({
-        message: err.message,
-        error: {},
-        title: 'error',
+      message: err.message,
+      error: err,
+      title: 'error',
     });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {},
+    title: 'error',
+  });
 });
 
 
