9115ad2b1e6d11c5076f6d4bbc9a46b39d878df9
bvaisakh <vaisakhb12@gmail.com>
2020-04-02 22:08:19 +0530

    Added terms static file

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const glob = require('glob');
+const path = require('path');
+const compress = require('compression');
+const morgan = require('morgan');
 const config = require('./utils/config');
 
 const app = express();
+const STATIC_DIR = path.join(config.ROOT, '/resources/public');
+const MORGAN_LOG_FORMAT =
+  ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent"';
+
+app.use(morgan(MORGAN_LOG_FORMAT));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(compress());
+app.use(express.static(STATIC_DIR));
 
 const controllers = glob.sync(config.ROOT + '/src/controllers/*.js');
 controllers.forEach(function (controller) {
